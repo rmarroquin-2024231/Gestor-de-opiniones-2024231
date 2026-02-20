2600c84ec1f25fc7a6a157b89f98fe113fe7c477
Informatica <informatica@alumno.local>
2026-02-20 15:56:41 -0600

    feat: Sprint 1 - módulo de autenticación y perfil con JWT, hash de contraseñas y rutas protegidas

---
+++ auth-service/src/AuthService.Domain/Constants/RoleConstants.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace AuthService.Domain.Constants;
+
+public class RoleConstants
+{
+    public const string USER_ROLE = "USER_ROLE";
+    public const string ADMIN_ROLE = "ADMIN_ROLE";
+    public static readonly string[] AllowedRoles = { USER_ROLE, ADMIN_ROLE };
+}
